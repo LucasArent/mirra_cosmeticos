7dacf09a821371cd0cf4149433967abfae5972cd
LucasArent <arentlucas2708@gmail.com>
2023-11-15 18:05:37 -0300

    done Catalogo, contact btn and slidbar

---
+++ script.js
@@ -0,0 +1,8 @@
+document.querySelector('.hamburger-menu').addEventListener('click', () => {
+    const slidebar = document.querySelector('.slidebar');
+    if (slidebar.style.display === 'block') {
+      slidebar.style.display = 'none';
+    } else {
+      slidebar.style.display = 'block';
+    }
+  });
