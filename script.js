e83adc0c794bdea24353dbcce753d43b59f03708
LucasArent <arentlucas2708@gmail.com>
2023-11-28 18:00:01 -0300

    add itens to calotogo, change the video and fix the thanks

--- script.js
+++
@@ -1,8 +0,0 @@
-// document.querySelector('.hamburger-menu').addEventListener('click', () => {
-//     const slidebar = document.querySelector('.slidebar');
-//     if (slidebar.style.display === 'block') {
-//       slidebar.style.display = 'none';
-//     } else {
-//       slidebar.style.display = 'block';
-//     }
-//   });
